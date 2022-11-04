7c41ea3f8974534ebc124c1590ee029a8b244f6b
SathvikY3533 <sathvik.y007@gmail.com>
2022-11-04 13:06:42 -0500

    Nav bar updated for radicubs website

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,15 @@
-var search = document.getElementById("search");
+//Search Bar Stuff ---------
+// var search = document.getElementById("search");
 
-search.addEventListener("keypress", function(event) {
-  // If the user presses the "Enter" key on the keyboard
-  if (event.key === "Enter") {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    alert("You searched: " + search.value);
-  }
-});
+// search.addEventListener("keypress", function(event) {
+//   // If the user presses the "Enter" key on the keyboard
+//   if (event.key === "Enter") {
+//     // Cancel the default action, if needed
+//     event.preventDefault();
+//     // Trigger the button element with a click
+//     alert("You searched: " + search.value);
+//   }
+// });
+//Search Bar Stuff ---------
+
+
