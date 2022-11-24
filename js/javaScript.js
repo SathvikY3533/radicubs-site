d9ee1619a230ca58a072af8f96b83ce964cb4cce
SathvikY3533 <sathvik.y007@gmail.com>
2022-11-24 12:06:20 -0600

    Updated Nav Bar
    made it look more INSANE by turning it to a button.

--- js/javaScript.js
+++ js/javaScript.js
@@ -12,7 +12,7 @@
 // });
 //Search Bar Stuff ---------
 
-const nav = document.getElementById("commonNav");
+const nav = document.getElementById("NavBar");
 
 const homeLink = document.getElementById('home');
 const homeCode = document.getElementById('homePage');
@@ -23,17 +23,49 @@ const sponsorsCode = document.getElementById('sponsorsPage');
 const teamLink = document.getElementById('team');
 const teamCode = document.getElementById('teamPage');
 
+const contactUsLink = document.getElementById('contact');
+const contactUsCode = document.getElementById('contactUsPage');
+
 
 homeLink.addEventListener("click", () => {
     homeCode.style.display = "block";
     sponsorsCode.style.display = "none";
-    nav.style.display = "none";
+    contactUsCode.style.display = "none";
+
+    // nav.style.display = "block";
 });
 
 sponsorsLink.addEventListener("click", () => {
     homeCode.style.display = "none";
     sponsorsCode.style.display = "block";
-    nav.style.display = "block";
+    contactUsCode.style.display = "none";
+
+    // nav.style.display = "block";
+    // nav.style = "margin: 0 28vw; margin-top: 2.5vw; text-align: center; text-color: white;";
 });
+
+contactUsLink.addEventListener("click", () => {
+    homeCode.style.display = "none";
+    sponsorsCode.style.display = "none";
+    contactUsCode.style.display = "block";
+
+    // nav.style.display = "block";
+    // nav.style = "margin: 0 28vw; margin-top: 2.5vw; text-align: center; color: white;";
+});
+
+
+
+
+function navBarSwitch(x) {
+    x.classList.toggle("change");
+}
+
+function openNav() {
+    document.getElementById("myNav").style.width = "45vw";
+}
+  
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
 
 
